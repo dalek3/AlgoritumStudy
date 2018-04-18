4d1fa0ac0680e290dcb5b392aa288c1407522599
lina1318 <lina1318@gmail.com>
2018-04-18 13:45:26 +0900

    번호 틀린거 조정 및 .java 파일만 놔두고 삭제(이름 변경)

--- BOJ/10950.java
+++ BOJ/10950.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class aPlusb3 {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
--- BOJ/2558.java
+++ BOJ/2558.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class aPlusb2 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = 0;
